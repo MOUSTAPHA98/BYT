b48dfd2288546ccd7a552108045d95226a1c4280
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2021-02-20 05:09:17 +0200

    ...

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 $("#gridToggler").on("click", function () { if ($("#gridToggler").prop('checked') === true){ $(".gridView").css("height", "100%") } else { $(".gridView").css("height", "0") } })
 
 
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.top-destinations-swiper', {
     centeredSlides: false,
     autoplay: {
     delay: 6000,
-    loop: true,
     },
     pagination: {
     el: '.swiper-pagination',
     clickable: true,
     },
     navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.top-dest-swiper-button-next',
+    prevEl: '.top-dest-swiper-button-prev',
     },
     breakpoints:{
         320: {
@@ -28,5 +27,20 @@ var swiper = new Swiper('.swiper-container', {
             spaceBetween: 30,
             slidesPerView: 3,
         }
-    }
+    },
+    loop: true,
+});
+
+// 1. Simple mode, it styles document scrollbar:
+$(function() {  
+    $("html").niceScroll();
+});
+
+$("html").niceScroll({
+    cursorcolor: "#e04622", // change cursor color in hex
+    scrollspeed: 500, // scrolling speed
+    mousescrollstep: 30, // scrolling speed with 
+    cursorwidth: "10px", // cursor width in pixel (you can also write "5px")
+    cursorborder: "0", // css definition for cursor border
+    smoothscroll: true, // scroll with ease movement
 });
