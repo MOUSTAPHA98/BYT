ec8892b922975aa2d619eac4c2c400ad2d1d0a29
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2021-02-21 06:36:35 +0200

    ...

--- dist/js/index.js
+++ dist/js/index.js
@@ -30,6 +30,36 @@ var swiper = new Swiper('.top-destinations-swiper', {
     loop: true,
 });
 
+var swiper = new Swiper('.testimonials-swiper', {
+    centeredSlides: false,
+    autoplay: {
+    delay: 5000,
+    },
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
+    navigation: {
+    nextEl: '.testimonials-swiper-button-next',
+    prevEl: '.testimonials-swiper-button-prev',
+    },
+    breakpoints:{
+        320: {
+            spaceBetween: 20,
+            slidesPerView: 1,
+        },
+        991: {
+            spaceBetween: 20,
+            slidesPerView: 2,
+        },
+        1199: {
+            spaceBetween: 30,
+            slidesPerView: 2,
+        }
+    },
+    loop: true,
+});
+
 // 1. Simple mode, it styles document scrollbar:
 $(document).ready(function () {
     if (window.innerWidth >= 1024){
